3b73c5992393ae5f546fe47641781a3e113e708a
Ömer Faruk Arslan <o.farukarslan0@gmail.com>
2021-03-15 11:34:19 +0300

    Update php05.php

--- AJAX - MySQLi Search/php05.php
+++ AJAX - MySQLi Search/php05.php
@@ -1,10 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <title>PHP - MySQL</title>
-</head>
-<body>
 <?php
     if (isset($_GET["q"])){
         $arama = $_GET["q"];
@@ -45,6 +38,3 @@
 
     $bag->close();
 ?>
-
-</body>
-</html>
